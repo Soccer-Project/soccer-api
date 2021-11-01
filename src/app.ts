d4053f991d0cdb5158b8de5b9a6ecd9fca7a825e
Nathally Souza <72207361+nathyts@users.noreply.github.com>
2021-11-01 17:09:23 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,14 @@ import createConnection from'./database';
 createConnection();
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.json());
 app.use(router)
