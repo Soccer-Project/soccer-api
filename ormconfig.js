38976a9523a37126756f6b363e44730a84f18777
Nathally Souza <nathallyccd@gmail.com>
2021-11-06 13:02:40 -0300

    Updating URL in package.json

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,16 @@
 module.exports = {
     "type": "mysql",
+    "url": `mysql2://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:3306/${process.env.DATABASE_NAME}?sslca=config/amazon-rds-ca-cert.pem`,
     "host": process.env.DATABASE_HOST,
     "port": 3306,
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
     "entities": [
-        `${process.env.NODE_ENV === "production" ? "./dist" : "./src" }/entities/*${process.env.NODE_ENV === "production" ? ".js" : ".ts"}`,
+        `${process.env.NODE_ENV === "production" ? "./build" : "./src" }/entities/*${process.env.NODE_ENV === "production" ? ".js" : ".ts"}`,
     ],
     "migrations": [
-        `${process.env.NODE_ENV === "production" ? "./dist" : "./src" }/database/migrations/*${process.env.NODE_ENV === "production" ? ".js" : ".ts"}`,
+        `${process.env.NODE_ENV === "production" ? "./build" : "./src" }/database/migrations/*${process.env.NODE_ENV === "production" ? ".js" : ".ts"}`,
     ],
     "cli": {
         "migrationsDir": "./src/database/migrations",
