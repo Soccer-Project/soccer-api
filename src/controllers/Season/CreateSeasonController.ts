6b68cc04d8ee4561414b4d85331368914d01d28f
Nathally Souza <nathally.souza_alves@bp.com>
2022-01-03 21:39:13 -0300

    Add type for functions in controllers/Season

--- src/controllers/Season/CreateSeasonController.ts
+++ src/controllers/Season/CreateSeasonController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { CreateSeasonService } from '../../services/Season/CreateSeasonService';
 
 class CreateSeasonController{
-    async handle(request: Request, response: Response){
+    async handle(request: Request, response: Response): Promise<Response> {
         const { name } = request.body
 
         const createSeasonService = new CreateSeasonService({name});
